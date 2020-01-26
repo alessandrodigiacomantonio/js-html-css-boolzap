c94fb8c4d674f554877e43112895f2e80eb2d4cf
alessandrodigiacomantonio <alessandrodigiacomantonio94@gmail.com>
2020-01-26 17:18:05 +0100

    Scritto template per i messaggi, fixate proporzioni varie, aggiunte chat

--- js/script.js
+++ js/script.js
@@ -1,20 +1,19 @@
 $(document).ready(function() {
   var input = $(".footer__searchbar > .searchbar__input");
-  console.log(input);
   input.on("keydown", function(event) {
     if (event.keyCode === 13) {
       event.preventDefault();
       var newMessage = $(".footer__searchbar > .searchbar__input").val();
       $(".footer__searchbar > .searchbar__input").val("");
-      var templateMessage = $(".primary__main > *:first-child").clone();
-      templateMessage.children().children().html(newMessage);
+      var templateMessage = $(".template-message").find('.clearfix').clone();
+      templateMessage.children().attr('data-type', 'send').children('.message__txt').text(newMessage);
       $('.primary__main').append(templateMessage);
-      var element = $(".primary__main");
-      element.scrollTop(10000);
+      var scrollbar = $(".primary__main");
+      scrollbar.scrollTop(10000);
       var timer = setTimeout(function () {
-        $('.primary__main').append(templateMessage);
-        var templateMessage = $(".primary__main > *:nth-of-type(2)").clone();
-        templateMessage.children().children().html('Øk');
+        // $('.primary__main').append(templateMessage);
+        var templateMessage = $(".template-message").find('.clearfix').clone();
+        templateMessage.children().attr('data-type','receive').children('.message__txt').text('Øk');
         $('.primary__main').append(templateMessage);
         var element = $(".primary__main");
         element.scrollTop(10000);
